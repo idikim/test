2740d5616ed6aa09b1817605646a86956b4a9b45
idikim <hjkim9217@naver.com>
2024-11-13 19:01:11 +0900

    map 메서드 연습3

--- test.dart
+++ test.dart
@@ -1,6 +1,12 @@
 void main() {
-  var list = [1, 2, 3];
-  var newList = list.map((e) => e + 1); // 각 요소(e)에 1을 더한다
-  print(list); // [1, 2, 3]
-  print(newList);
+  List<int> list = [1, 2, 3, 4, 5];
+
+  var mappedIterable = list.map(
+    (e) {
+      return e * 10;
+    },
+  );
+
+  mappedIterable.toList();
+  print(mappedIterable);
 }
